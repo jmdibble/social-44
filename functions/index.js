067fdf8069e5c2d95dfd3a280fc0987e54fe45a5
Josh <joshdibble93@gmail.com>
2019-08-11 11:11:38 +0100

    Refactored all routes and handlers into util

--- functions/index.js
+++ functions/index.js
@@ -1,204 +1,22 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp();
+const app = require('express')();
 
-const config = {
-  apiKey: 'AIzaSyBLBM3vjgkDzjKwJvFJsqx0rEhe7Uj2Iuk',
-  authDomain: 'social-44.firebaseapp.com',
-  databaseURL: 'https://social-44.firebaseio.com',
-  projectId: 'social-44',
-  storageBucket: 'social-44.appspot.com',
-  messagingSenderId: '770034608691',
-  appId: '1:770034608691:web:2eef1403de96fc06'
-};
+const FBAuth = require('./util/fbAuth');
 
-const firebase = require('firebase');
-firebase.initializeApp(config);
+const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { signup, login } = require('./handlers/users');
 
-const db = admin.firestore();
-
-const express = require('express');
-const app = express();
-
-app.get('/screams', (req, res) => {
-  db.collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then(data => {
-      let screams = [];
-      data.forEach(doc => {
-        screams.push({
-          screamId: doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt
-        });
-      });
-      return res.json(screams);
-    })
-    .catch(err => console.error(err));
-});
-
-const FBAuth = (req, res, next) => {
-  let idToken;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer ')
-  ) {
-    idToken = req.headers.authorization.split('Bearer ')[1];
-  } else {
-    console.error('No token found');
-    return res.status(403).json({ error: 'Unauthorized' });
-  }
-
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then(decodedToken => {
-      req.user = decodedToken;
-      console.log(decodedToken);
-      return db
-        .collection('users')
-        .where('userId', '==', req.user.uid)
-        .limit(1)
-        .get();
-    })
-    .then(data => {
-      req.user.handle = data.docs[0].data().handle;
-      return next();
-    })
-    .catch(err => {
-      console.error('Error while wrifying token', err);
-      return res.status(403).json(err);
-    });
-};
-
+// screams.js
+// All screams
+app.get('/screams', getAllScreams);
 // Post one scream
-app.post('/scream', FBAuth, (req, res) => {
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.user.handle,
-    createdAt: new Date().toISOString()
-  };
-  db.collection('screams')
-    .add(newScream)
-    .then(doc => {
-      res.json({ message: `Document ${doc.id} created successfully` });
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'Something went wrong' });
-      console.error(err);
-    });
-});
+app.post('/scream', FBAuth, postOneScream);
 
-const isEmpty = string => {
-  if (string.trim() === '') return true;
-  else return false;
-};
-
-const isEmail = email => {
-  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(regEx)) return true;
-  else return false;
-};
-
+// users.js
 // Signup route
-app.post('/signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle
-  };
-
-  // Validation
-  let errors = {};
-
-  if (isEmpty(newUser.email)) {
-    errors.email = 'Must not be empty';
-  } else if (!isEmail(newUser.email)) {
-    errors.email = 'Must be a valid email address';
-  }
-
-  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
-  if (newUser.password !== newUser.confirmPassword)
-    errors.confirmPassword = 'Passwords must match';
-  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  // Validate data
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then(doc => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: 'This handle is already taken' });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
-    .then(data => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then(idToken => {
-      token = idToken;
-      const userCredentials = {
-        handle: newUser.handle,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId
-      };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(() => {
-      return res.status(201).json({ token });
-    })
-    .catch(err => {
-      console.error(err);
-      if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already in use' });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
-    });
-});
-
+app.post('/signup', signup);
 // Login route
-app.post('/login', (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  };
-
-  let errors = {};
-
-  if (isEmpty(user.email)) errors.email = 'Must not be empty';
-  if (isEmpty(user.password)) errors.password = 'Must not be empty';
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(user.email, user.password)
-    .then(data => {
-      return data.user.getIdToken();
-    })
-    .then(token => {
-      return res.json({ token });
-    })
-    .catch(err => {
-      console.error(err);
-      if (err.code === 'auth/wrong-password') {
-        return res
-          .status(403)
-          .json({ general: 'Wrong credentials, please try again' });
-      } else return res.status(500).json({ error: err.code });
-    });
-});
+app.post('/login', login);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
