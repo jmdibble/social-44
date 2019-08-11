0483a640d25f5d4b4112a05508d5b8440f9f8a9f
Josh <joshdibble93@gmail.com>
2019-08-11 16:21:53 +0100

    Created add comment route

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -7,5 +7,39 @@ let db = {
       likeCount: 5,
       commentCount: 2
     }
+  ],
+
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.789Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'Lonodn, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa'
+    },
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO'
+    }
   ]
 };
--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,31 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require('./handlers/screams');
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require('./handlers/users');
 
 // screams.js
 // All screams
 app.get('/screams', getAllScreams);
 // Post one scream
 app.post('/scream', FBAuth, postOneScream);
+// Get screams
+app.get('/scream/:screamId', getScream);
+// Add comment
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+
+// TODO: delete scream, like scream, unlike scream, comment on scream
 
 // users.js
 // Signup route
@@ -20,5 +37,9 @@ app.post('/signup', signup);
 app.post('/login', login);
 // Image upload
 app.post('/user/image', FBAuth, uploadImage);
+// Optional fields
+app.post('/user', FBAuth, addUserDetails);
+// Like route
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
