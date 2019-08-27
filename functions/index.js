31f8e9fe92f9aa9ac56a0c1e0e67371bc715e786
Josh <joshdibble93@gmail.com>
2019-08-27 22:24:05 +0100

    Backend finished

--- functions/index.js
+++ functions/index.js
@@ -131,7 +131,7 @@ exports.onUserImageChange = functions
     console.log(change.after.data());
     if (change.before.data().imageUrl !== change.after.data().imageUrl) {
       console.log('Image has changed');
-      let batch = db.batch();
+      const batch = db.batch();
       return db
         .collection('screams')
         .where('userHandle', '==', change.before.data().handle)
@@ -143,5 +143,42 @@ exports.onUserImageChange = functions
           });
           return batch.commit();
         });
-    }
+    } else return true;
+  });
+
+exports.onScreamDelete = functions
+  .region('europe-west1')
+  .firestore.document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db
+          .collection('likes')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch(err => console.error(err));
   });
